1981345e77f3874f4ee21ae9b29373c5f7628b9c
TheLivingMango <oconne.ben@awatapu.school.nz>
2021-07-06 14:50:34 +1200

    Made a basic Yes / No Checker updated v2

--- venv/01_yes_no_checker.py
+++ venv/01_yes_no_checker.py
@@ -1,12 +1,14 @@
-show_instructions = input("Have you played this game before?").lower()
+= show_instructions == ""
+while show_instructions.lower() !="XXX":
+    show_instructions = input("Have you played this game before?").lower()
 
-if show_instructions == "yes":
+if show_instructions == "yes" or show_instructions == "y":
     print("program Continues")
 
 elif show_instructions == "y":
     print("program continues")
 
-elif show_instructions == "no":
+elif show_instructions == "no" or show_instructions == "n":
     print("display instructions")
 
 elif show_instructions == "n":
