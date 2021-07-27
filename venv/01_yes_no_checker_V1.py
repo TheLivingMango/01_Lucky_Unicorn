114ac5c42deff8e267fc197fb371b503783c1646
TheLivingMango <oconne.ben@awatapu.school.nz>
2021-07-27 13:21:07 +1200

    Yes no checker V3

---
+++ venv/01_yes_no_checker_V1.py
@@ -0,0 +1,20 @@
+show_instructions == "response"
+while show_instructions.lower() !="response":
+    show_instructions = input("Have you played this game before?").lower()
+
+if show_instructions == "yes" or show_instructions == "y":
+    print("program Continues")
+
+elif show_instructions == "y":
+    print("program continues")
+
+elif show_instructions == "no" or show_instructions == "n":
+    print("display instructions")
+
+elif show_instructions == "n":
+    print("display instructions")
+
+else:
+    print("Please answer with y / n")
+
+
--- venv/this_name_idk2.py
+++
@@ -1,1 +0,0 @@
-print("Hello This is a test to see if this will work.")
