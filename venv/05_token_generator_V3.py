51dc29979502f9b5b782ac1d09151657d8b1f083
TheLivingMango <oconne.ben@awatapu.school.nz>
2021-08-13 09:30:37 +1200

    Version 3 of Token Generator debugged

---
+++ venv/05_token_generator_V3.py
@@ -0,0 +1,24 @@
+import random
+
+tokens = ["unicorn", "horse", "zebra", "donkey"]
+STARTING_BALANCE = 100
+
+balance = STARTING_BALANCE
+
+chosen = random.choice(tokens)
+print(chosen)
+for item in range(0, 100):
+
+    print(chosen, end=' t ')
+
+    if chosen == "unicorn":
+        balance += 4
+
+    elif chosen == "donkey":
+        balance -= 1
+
+    else:
+        balance -= 0.5
+
+    print("Starting Balance ${:.2f}".format(STARTING_BALANCE))
+    print("Final Balance ${:.2f}".format(balance))
