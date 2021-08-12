bfb655092d52f5ab4af60a0100aedda27f374c67
TheLivingMango <oconne.ben@awatapu.school.nz>
2021-08-13 09:29:15 +1200

    Version 1 of Token Generator debugged

--- venv/05_token_generator_V1.py
+++ venv/05_token_generator_V1.py
@@ -1,8 +1,11 @@
 import random
 
 tokens = ["unicorn", "horse", "zebra", "donkey"]
+
 chosen = random.choice(tokens)
 print(chosen)
+
 for item in range(0,20):
     chosen = random.choice(tokens)
+
     print(chosen, end=' t ')
