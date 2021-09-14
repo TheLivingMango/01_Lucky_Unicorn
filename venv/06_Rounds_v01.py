cf15acae4c206b18027d8cd15501f70474416441
TheLivingMango <oconne.ben@awatapu.school.nz>
2021-09-14 15:01:02 +1200

    Version 01 of Round count

---
+++ venv/06_Rounds_v01.py
@@ -0,0 +1,22 @@
+
+balance = 5
+
+rounds_played = 0
+
+play_again = input("Press <Enter> to play...")
+while play_again == "":
+
+    rounds_played += 1
+    print("***Round #{}***".format(rounds_played))
+    balance -= 1
+    print("Balance:", balance)
+    print()
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+        play_again = input("Press <Enter> to play again"
+                           "or 'xxx' to quit")
+
+print()
+print("Final Balance:", balance)
