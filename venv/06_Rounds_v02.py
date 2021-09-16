54f8842b54407f46ce21e32192ccfd163567008a
TheLivingMango <oconne.ben@awatapu.school.nz>
2021-09-17 10:16:32 +1200

    Version 02 of Round count completed

---
+++ venv/06_Rounds_v02.py
@@ -0,0 +1,42 @@
+import random
+
+balance = how_much
+
+
+rounds_played = 0
+
+play_again = input("Press <Enter> to play...")
+
+while play_again == "":
+
+    rounds_played += 1
+    print("***Round #{}***".format(rounds_played))
+
+    chosen_num = random.randint(1, 100)
+
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+
+    else:
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+            balance -= 0.5
+        else:
+            chosen = "zebra"
+            balance -= 0.5
+
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+
+    else:
+        play_again = input("Press <Enter> to play again"
+                           " or 'xxx' to quit")
+
+print()
+print("Final Balance:", balance)
